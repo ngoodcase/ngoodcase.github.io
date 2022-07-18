0550a515255e14e6a9549e48ed455eabee64f695
ngoodcase <107001465+ngoodcase@users.noreply.github.com>
2022-07-18 11:44:03 -0500

    Canvas

--- Canvas.js
+++ Canvas.js
@@ -3,12 +3,13 @@ var canvas = document.querySelector('canvas')
 var c = canvas.getContext('2d');
 
 c.fillStyle = 'blue';
-c.fillRect(100, 100, 100, 100)
+c.fillRect(110, 75, 500, 50)
 c.fillStyle = 'cyan';
-c.fillRect(210,100,100,100)
+c.fillRect(110, 275, 500, 50)
 console.log('canvas');
 
 // Line
+c.strokeStyle ='red'
 c.beginPath();
 c.moveTo(50, 100);
 c.lineTo(50, 300);
@@ -18,9 +19,20 @@ c.beginPath();
 c.moveTo(50, 100);
 c.lineTo(100, 200);
 c.stroke()
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(100, 200);
+c.stroke()
+
 
 // Arc / Circle
+c.strokeStyle ='black'
 c.beginPath();
-c.arc(300, 300, 300, 30, 0 Math.PI * 2, false);
+c.arc(50, 100, 50, 0, Math.PI * 2, false);
+c.stroke();
+// Arc / Circle
+c.beginPath();
+c.arc(50, 300, 50, 0, Math.PI * 2, false);
 c.stroke();
 

ee6abf34cf6ba91856dd04199146180d097fdafe
ngoodcase <107001465+ngoodcase@users.noreply.github.com>
2022-07-18 09:46:57 -0500

    Canvas

--- Canvas.js
+++ Canvas.js
@@ -1,12 +1,9 @@
 window.addEventListener("load", () => { 
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContex("2d");
+    const canvas = document.querySelector("#Canvas.js");
+    const ctx = canvas.getContex("2d"); 
 
-    //resizing
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;  
+    ctx.strokeStyle = "red";    
+    ctx.fillRect(100, 100, 200, 500);
+
+    });
     
-    ctx.fillRect(50, 50, 200, 200);
-});
-
-window.addEventListener('resize, ')

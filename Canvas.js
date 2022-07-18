ff6d086c625d7434a727e9b0aa0bb474cd4ba85e
ngoodcase <107001465+ngoodcase@users.noreply.github.com>
2022-07-18 09:57:47 -0500

    CANVAS

--- Canvas.js
+++ Canvas.js
@@ -1,9 +1,3 @@
-window.addEventListener("load", () => { 
-    const canvas = document.querySelector("#Canvas.js");
-    const ctx = canvas.getContex("2d"); 
-
-    ctx.strokeStyle = "red";    
-    ctx.fillRect(100, 100, 200, 500);
-
-    });
-    
+var canvas = document.querySelector('canvas')
+console.log('canvas');
+canvas.width = window.innerWidth;

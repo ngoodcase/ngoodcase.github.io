b95e3190f978b670db782ae2fdeb601b385455ed
ngoodcase <107001465+ngoodcase@users.noreply.github.com>
2022-07-18 10:59:44 -0500

    Canvas

--- Canvas.js
+++ Canvas.js
@@ -1,2 +1,19 @@
 var canvas = document.querySelector('canvas')
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100)
+c.fillRect(210,100,100,100)
 console.log('canvas');
+
+// Line
+c.beginPath();
+c.moveTo(50, 100);
+c.lineTo(50, 300);
+c.stroke()
+// Line
+c.beginPath();
+c.moveTo(50, 100);
+c.lineTo(100, 200);
+c.stroke()
+
